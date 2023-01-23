345617418d60d2fb68dd31a5609a7a5359901433
yusuf islam demir <yusufislamdemir41@gmail.com>
2023-01-23 21:05:29 +0300

    edit main method

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Service service = new Service();
-        System.out.println(service.getHousesValues());
-        System.out.println(service.getVillasValues());
-        System.out.println(service.getSummeriesValues());
-        System.out.println(service.getHousesTotalPrice());
-        System.out.println(service.getVillasTotalPrice());
-        System.out.println(service.getSummeryTotalPrice());
-        System.out.println(service.getTotalPriceForAll());
-        System.out.println(service.getAverageSquareMeterForHouses());
-        System.out.println(service.getAverageSquareMeterForVillas());
-        System.out.println(service.getAverageSquareMeterForSummeries());
-        System.out.println(service.getAverageSquareMeterForAll());
-        System.out.println(service.getByNumberOfRoomAndHall(4,3));
+        System.out.println(service.getHousesValues() + " tüm evlerin bilgileri");
+        System.out.println(service.getVillasValues() + " tüm villaların bilgileri");
+        System.out.println(service.getSummeriesValues() + " tüm yazlıkların bilgileri ");
+        System.out.println(service.getHousesTotalPrice() + " evlerin toplam fiyatı");
+        System.out.println(service.getVillasTotalPrice() + " villaların toplam fiyatı");
+        System.out.println(service.getSummeryTotalPrice() + " yazlıkların toplam fiyatı");
+        System.out.println(service.getTotalPriceForAll() + " tüm tipteki evlerin toplam fiyatı");
+        System.out.println(service.getAverageSquareMeterForHouses() + " evlerin ortalama metrekaresi");
+        System.out.println(service.getAverageSquareMeterForVillas() + " villaların ortalama metrekaresi");
+        System.out.println(service.getAverageSquareMeterForSummeries() + " yazlıkların ortalama metrekaresi");
+        System.out.println(service.getAverageSquareMeterForAll() + " tüm tipteki evlerin ortalama metrekaresi");
+        System.out.println(service.getByNumberOfRoomAndHall(4, 3) + " tüm tipteki evlerin oda ve salon sayısına göre getirilmesi");
     }
 }

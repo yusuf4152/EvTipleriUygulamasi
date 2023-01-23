feb2cd4830df7d5683285a52f446899d948b704d
yusuf islam demir <yusufislamdemir41@gmail.com>
2023-01-23 20:58:01 +0300

    added commentLines

--- src/Creation.java
+++ src/Creation.java
@@ -7,9 +7,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Creation {
-    private final List<House> houses= new ArrayList<>();
-    private final List<Villa> villas= new ArrayList<>();
-    private final List<Summery> summeries= new ArrayList<>();
+    private final List<House> houses = new ArrayList<>();
+    private final List<Villa> villas = new ArrayList<>();
+    private final List<Summery> summeries = new ArrayList<>();
     private final House house1 = new House(200000, 120, 2, 1);
     private final House house2 = new House(150000, 110, 2, 1);
     private final House house3 = new House(250000, 130, 2, 1);
@@ -20,16 +20,20 @@ public class Creation {
     private final Summery summery2 = new Summery(202000, 230, 3, 2);
     private final Summery summery3 = new Summery(203000, 240, 3, 2);
 
+    //olusturulan evleri döner
     public List<House> getHouseList() {
         houses.addAll(Arrays.asList(house1, house2, house3));
         return houses;
     }
 
+    //olusturulan villaları döner
     public List<Villa> getVillaList() {
         villas.addAll(Arrays.asList(villa1, villa2, villa3));
         return villas;
     }
 
+
+    //olusturulan yazlıkları döner
     public List<Summery> getSummeryList() {
         summeries.addAll(Arrays.asList(summery1, summery2, summery3));
         return summeries;
--- src/Service.java
+++ src/Service.java
@@ -18,55 +18,75 @@ public class Service {
     private int villaTotalSquareMeter = 0;
     private int summeryTotalSquareMeter = 0;
 
+
+    //olusturulan evlerin bilgilerini döner
     public List<String> getHousesValues() {
         return houseList.stream().map(House::toString).collect(Collectors.toList());
     }
 
+
+    //olusturulan villaların bilgilerini döner
     public List<String> getVillasValues() {
         return villaList.stream().map(Villa::toString).collect(Collectors.toList());
     }
 
+
+    //olusturulan yazlıkların bilgilerini döner
     public List<String> getSummeriesValues() {
         return summeryList.stream().map(Summery::toString).collect(Collectors.toList());
     }
 
+
+    //evlerin toplam fiyatını döner
     public int getHousesTotalPrice() {
         return houseList.stream().mapToInt(BaseHome::getPrice).sum();
     }
 
+
+    //villaların toplam fiyatını döner
     public int getVillasTotalPrice() {
         return villaList.stream().mapToInt(BaseHome::getPrice).sum();
     }
 
+
+    //yazlıkların toplam fiyatını döner
     public int getSummeryTotalPrice() {
         return summeryList.stream().mapToInt(BaseHome::getPrice).sum();
     }
 
+    // tüm tipteki evlerin topma fiyatını döner
     public int getTotalPriceForAll() {
         return getSummeryTotalPrice() + getHousesTotalPrice() + getVillasTotalPrice();
     }
 
+    //evlerin ortalama metrekaresini döner
     public int getAverageSquareMeterForHouses() {
         houseTotalSquareMeter = houseList.stream().mapToInt(BaseHome::getSquareMeters).sum();
         return houseList.stream().mapToInt(BaseHome::getSquareMeters).sum() / houseList.size();
     }
 
+
+    //villaların ortalama metrekaresini döner
     public int getAverageSquareMeterForVillas() {
         villaTotalSquareMeter = villaList.stream().mapToInt(BaseHome::getSquareMeters).sum();
         return villaList.stream().mapToInt(BaseHome::getSquareMeters).sum() / houseList.size();
     }
 
+
+    //yazlıların ortalama metrekaresini döner
     public int getAverageSquareMeterForSummeries() {
         summeryTotalSquareMeter = summeryList.stream().mapToInt(BaseHome::getSquareMeters).sum();
         return summeryList.stream().mapToInt(BaseHome::getSquareMeters).sum() / houseList.size();
     }
 
+    //tüm tipteki evlerin ortalama metrekaresini döner
     public int getAverageSquareMeterForAll() {
         int sizes = houseList.size() + villaList.size() + summeryList.size();
         int total = houseTotalSquareMeter + villaTotalSquareMeter + summeryTotalSquareMeter;
         return total / sizes;
     }
 
+    //oda ve salon sayısına göre tüm tipteki evleri getirir
     public List<String> getByNumberOfRoomAndHall(int numberOfRoom, int numberOfHall) {
         List<String> selectedHomes = new ArrayList<>();
         houseList.stream()
